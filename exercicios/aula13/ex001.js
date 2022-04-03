53325792b23bda4fcaaaba5a4ec91fe8ce70fe4e
Darling Cristiane <darlingcristianes@gmail.com>
2022-04-03 18:57:17 -0300

    exercicio13

---
+++ exercicios/aula13/ex001.js
@@ -0,0 +1,14 @@
+var contador = 1
+
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 10)
+
+
+/*while (contador <= 100) {
+      console.log(`Passo ${contador}`)
+}
+/*c=c+1*/
+
+/*console.log('Tudo bem?')*/
