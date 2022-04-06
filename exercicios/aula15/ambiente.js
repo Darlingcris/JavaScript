ee6aac7f2fc23525d97004a05ed73a3e1ef58281
Darling Cristiane <darlingcristianes@gmail.com>
2022-04-06 20:14:49 -0300

    vetores

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,22 @@
+/*let num = [5, 8, 2, 9, 3]
+console.log(`Nosso vetor é o  ${num}`)
+num[5]=12
+console.log(`Nosso vetor é o  ${num}`)
+num.push(7)
+console.log(`Nosso vetor é o  ${num}`)
+num.length
+console.log(`Nosso vetor é o  ${num}`)
+num.sort()
+console.log(`Nosso vetor é o  ${num}`)
+console.log(num)*/
+
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num[0] = 6
+num[6] = 7
+console.log(`Nosso vetor é o  ${num[3]}`)
+num.sort()
+console.log(num)
+
+console.log(`Os valores do vetor são ${num.length}`)
+console.log(`Nosso vetor é ${num}`)
---
+++ exercicios/aula15/vetorNaTela.js
@@ -0,0 +1,26 @@
+let valores = [5, 6, 2, 9, 3, 1]
+
+
+console.log(`A posição tem o indice ${valores.indexOf(9)}`)
+console.log(`Os valores em ordem crescente são ${valores.sort()}`)
+
+/*
+    valores.sort()
+console.log(valores)*/
+
+
+
+/*for (let pos = 0; pos <
+    valores.length; pos++) {
+    console.log(`A posição tem o valor ${valores[pos]}`)
+}*/
+
+/*var c = 0
+while (c < valores.length) {
+    console.log(`A posição tem o valor ${valores[c]}`)
+    c++
+}*/
+
+/*for (var c in valores) {
+    console.log(`A posição ${c} tem o valor ${valores[c]}`)
+}*/
